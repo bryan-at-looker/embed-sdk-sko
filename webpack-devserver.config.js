45c5eec5a711cdc422989d5111a2e0b634ea4792
Bryan Weber <bryanweber@15er.local>
2020-01-19 11:17:56 -0500

    first commit

---
+++ webpack-devserver.config.js
@@ -0,0 +1,54 @@
+var path = require('path')
+var config = require ('./config')
+
+var user = require('./demo/demo_user.json')
+var { createSignedUrl } = require('./server_utils/auth_utils')
+
+var webpackConfig = {
+  mode: 'development',
+  entry: {
+    demo: './demo/demo.ts'
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "demo")
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      config: path.join(__dirname, './config.js'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          compilerOptions: {
+            declaration: false
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    contentBase: [
+      path.join(__dirname, "demo")
+    ],
+    host: config.demo_host,
+    port: config.demo_port,
+    watchContentBase: true,
+    before: (app) => {
+      app.get('/auth', function(req, res) {
+        // Authenticate the request is from a valid user here
+        const src = req.query.src;
+        const url = createSignedUrl(src, user, config.host, config.secret);
+        res.json({ url });
+      });
+    }
+  }
+}
+
+module.exports = webpackConfig
