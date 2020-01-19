36527b9c3a686d96bed53e0b567ffd924dfafa6d
Bryan Weber <bryanweber@15er.local>
2020-01-19 14:08:37 -0500

    bare bones

--- demo/demo.ts
+++ demo/demo.ts
@@ -24,60 +24,19 @@ import { LookerEmbedSDK, LookerEmbedLook, LookerEmbedDashboard } from '../src/in
  * THE SOFTWARE.
  */
 
-import { lookerHost, dashboardId, lookId } from './demo_config'
+import { lookerHost, dashboardId } from './demo_config'
 
 LookerEmbedSDK.init(lookerHost, '/auth')
 
 const setupDashboard = (dashboard: LookerEmbedDashboard) => {
-  const runButton = document.querySelector('#run')
-  if (runButton) {
-    runButton.addEventListener('click', () => dashboard.run())
-  }
-  const stateFilter = document.querySelector('#state')
-  if (stateFilter) {
-    stateFilter.addEventListener('change', (event) => {
-      dashboard.updateFilters({ 'State / Region': (event.target as HTMLSelectElement).value })
-    })
-  }
-}
 
-const setupLook = (look: LookerEmbedLook) => {
-  const runButton = document.querySelector('#run')
-  if (runButton) {
-    runButton.addEventListener('click', () => look.run())
-  }
-  const stateFilter = document.querySelector('#state')
-  if (stateFilter) {
-    stateFilter.addEventListener('change', (event) => {
-      look.updateFilters({ 'users.state': (event.target as HTMLSelectElement).value })
-    })
-  }
-}
-
-const updateState = (selector: string, state: string) => {
-  const dashboardState = document.querySelector(selector)
-  if (dashboardState) {
-    dashboardState.textContent = state
-  }
-}
-
-const canceller = (event: any) => {
-  updateState('#dashboard-state', `${event.label} clicked`)
-  return { cancel: !event.modal }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   if (dashboardId) {
     LookerEmbedSDK.createDashboardWithId(dashboardId)
       .appendTo('#dashboard')
-      .on('dashboard:run:start', () => updateState('#dashboard-state', 'Running'))
-      .on('dashboard:run:complete', () => updateState('#dashboard-state', 'Done'))
-      .on('drillmenu:click', canceller)
-      .on('drillmodal:explore', canceller)
-      .on('dashboard:tile:explore', canceller)
-      .on('dashboard:tile:view', canceller)
       .withClassName('looker-embed')
-      .withFilters({ 'State / Region': 'California' })
       .build()
       .connect()
       .then(setupDashboard)
@@ -88,20 +47,4 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector<HTMLDivElement>('#demo-dashboard')!.style.display = 'none'
   }
 
-  if (lookId) {
-    LookerEmbedSDK.createLookWithId(lookId)
-      .appendTo('#look')
-      .on('look:run:start', () => updateState('#look-state', 'Running'))
-      .on('look:run:complete', () => updateState('#look-state', 'Done'))
-      .withClassName('looker-embed')
-      .withFilters({ 'users.state': 'California' })
-      .build()
-      .connect()
-      .then(setupLook)
-      .catch((error: Error) => {
-        console.error('Connection error', error)
-      })
-  } else {
-    document.querySelector<HTMLDivElement>('#demo-look')!.style.display = 'none'
-  }
 })
--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -1,8 +1,4 @@
 // The address of your Looker instance. Required.
 export const lookerHost = 'saleseng.dev.looker.com'
-
 // A dashboard that the user can see. Set to 0 to disable dashboard.
 export const dashboardId = 715
-// A Look that the user can see. Set to 0 to disable look.
-export const lookId = 0
-
