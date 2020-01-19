5b85083c35b783cd4589723d6e32a36fd804b331
Bryan Weber <bryanweber@15er.local>
2020-01-19 14:34:34 -0500

    blank

--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -1,4 +1,4 @@
 // The address of your Looker instance. Required.
 export const lookerHost = 'saleseng.dev.looker.com'
 // A dashboard that the user can see. Set to 0 to disable dashboard.
-export const dashboardId = 715
+export const dashboardId = 0
