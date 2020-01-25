b81aafe9abfc3fd8f82a6b3a28d484a5a638625a
Bryan Weber <bryanweber@15er.local>
2020-01-25 12:59:55 -0500

    reskinned for section5

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,8 +1,8 @@
 import { LookerEmbedSDK, LookerEmbedDashboard } from '../src/index'
-import { lookerHost, dashboardId, dashboardStateFilter, query_object, query_field_name, dashboard_date_filter, query_date_filter } from './demo_config'
+import { looker_host, dashboard_id, dashboard_state_filter, query_object, query_field_name, dashboard_date_filter, query_date_filter, logoUrl, query_calculation } from './demo_config'
 import { LookerSDK, IApiSettings, AuthToken, IError, CorsSession } from '@looker/sdk'
 
-LookerEmbedSDK.init(lookerHost, '/auth')
+LookerEmbedSDK.init(looker_host, '/auth')
 document.addEventListener('DOMContentLoaded', embedSdkInit)
 
 let sdk: LookerSDK
@@ -15,16 +15,17 @@ class EmbedSession extends CorsSession {
 }
 
 const session = new EmbedSession({
-  base_url: `https://${lookerHost}:19999`,
+  base_url: `https://${looker_host}:19999`,
   api_version: '3.1'
 } as IApiSettings)
 sdk = new LookerSDK(session)
 
 const setupDashboard = async (dashboard: LookerEmbedDashboard) => {
+  
   const dropdown = document.getElementById('select-dropdown')
   if (dropdown) {
     dropdown.addEventListener('change', (event) => { 
-      dashboard.updateFilters({ [dashboardStateFilter]: (event.target as HTMLSelectElement).value }) 
+      dashboard.updateFilters({ [dashboard_state_filter]: (event.target as HTMLSelectElement).value })
       dashboard.run()
     })
   }
@@ -38,17 +39,22 @@ const setupDashboard = async (dashboard: LookerEmbedDashboard) => {
     }
   ))
   addStateOptions(states)
+  loadingIcon(false)
 }
 
 function embedSdkInit () {
-  if (dashboardId) {
-    LookerEmbedSDK.createDashboardWithId(dashboardId)
+  const logo = document.getElementById('logo')
+  if (logo) { logo.setAttribute('src',logoUrl)}
+  if (dashboard_id) {
+    LookerEmbedSDK.createDashboardWithId(dashboard_id)
       .appendTo('#dashboard')
       .withClassName('looker-embed')
-      .withFilters({[dashboardStateFilter]: 'New York'})
+      // .withFilters({[dashboard_date_filter]: '30 days'})
       .withTheme( 'sko' )
-      .on('page:properties:changed', changeHeight )
+      .on('page:property:change', changeHeight ) // dashboards-next
+      // .on('page:properties:changed', changeHeight ) // dashboards
       .on('dashboard:filters:changed', filtersUpdates)
+      .withNext()
       .build()
       .connect()
       .then(setupDashboard)
@@ -61,6 +67,7 @@ function embedSdkInit () {
 }
 
 async function filtersUpdates( event: any ) {
+  loadingIcon(true);
   console.log(event)
   // instantiate elements, filters, and query objects
   const dashboard_filters: any = (event && event.dashboard && event.dashboard.dashboard_filters) ? event.dashboard && event.dashboard.dashboard_filters : undefined
@@ -68,7 +75,6 @@ async function filtersUpdates( event: any ) {
   let new_filters = query_object.filters
 
   // update query object and run query
-  
   if (dashboard_filters && ( dashboard_date_filter in dashboard_filters ) ) { // check to make sure our filter is in the changed
     if (dropdown) { // check to make sure we found our elements to update/keep
       new_filters = Object.assign(new_filters, { [query_date_filter]: dashboard_filters[dashboard_date_filter] })
@@ -81,25 +87,24 @@ async function filtersUpdates( event: any ) {
       addStateOptions(states)
     }
   }
+  loadingIcon(false)
 }
 
 function changeHeight( event: any ) {
   console.log(event)
   const div = document.getElementById('dashboard')
   if (event && event.height && div) {
-    div.style.height = `${event.height}px`
+    div.style.height = `${event.height+20}px`
   }
 }
 
 function addStateOptions(data: any) {
-  const dropdown = document.getElementById('select-dropdown')
+  const dropdown = document.getElementById('dropdown-menu')
   if (dropdown && data && data.length > 1) {
     data.forEach(function (row: any, i: number) {
       if (query_field_name && row[query_field_name]) {
-        // create new option
-        const new_option = document.createElement('option')
-        new_option.value = row[query_field_name]
-        new_option.innerHTML = `${i+1}) ${row[query_field_name]}`
+        // create new option: replaced with function for more complex dropdown
+        const new_option = dropdownItem(row, i)
         // replace or create the dropdown item
         if (dropdown.children[i+1]) {
           dropdown.children[i+1].replaceWith(new_option)
@@ -108,5 +113,25 @@ function addStateOptions(data: any) {
         } 
       }
     })
+  }
+}
+
+
+function dropdownItem (row: any, i: number) {
+  const item = document.createElement('div')
+  item.setAttribute('data-value',row[query_field_name])
+  item.classList.add('item')
+  const options = (row[query_calculation] > 0) ? ['green','▲'] : (row[query_calculation] < 0) ? ['red','▼'] : ['black','']
+  const format = Number(row[query_calculation]).toLocaleString(undefined,{ style: 'percent', minimumFractionDigits: 2 })
+  item.innerHTML = `<span>${i+1}) ${row[query_field_name]}</span><span style='float: right;'><font color="${options[0]}">${options[1]} ${format}</font></span>`
+  return item
+}
+
+function loadingIcon (loading: boolean) {
+  const loader = document.getElementById('dropdown-icon-loader')
+  const icon = document.getElementById('dropdown-icon')
+  if (loader && icon) {
+    icon.style.display = (loading) ? 'none' : ''
+    loader.style.display = (loading) ? '' : 'none'
   }
 }
--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -1,20 +1,52 @@
 // The address of your Looker instance. Required.
-export const lookerHost = 'sko2020.dev.looker.com'
+export const looker_host = 'sko2020.dev.looker.com'
 // A dashboard that the user can see. Set to 0 to disable dashboard.
-export const dashboardId = 2
+export const dashboard_id = 5
 // the name of the filter on the dashboard for state
-export const dashboardStateFilter = 'State'
+export const dashboard_state_filter = 'State'
 // api call to run for state dropdown
 export const query_object = {
   "model": "thelook",
   "view": "order_items",
-  "fields": ["users.state", "order_items.total_gross_margin"],
-  "sorts": ["order_items.total_gross_margin desc"],
+  "fields": [
+    "this_period",
+    "previous_period",
+    "users.state",
+    "period_over_period"
+  ],
+  "filters": {
+    "order_items.previous_period_filter": "30 days",
+    "order_items.count": ">10"
+  },
+  "sorts": ["this_period desc"],
   "limit": "10",
-  "filters": {}
+  "dynamic_fields": "[{\"measure\":\"this_period\",\"based_on\":\"order_items.total_sale_price\",\"label\":\"This Period\",\"value_format\":null,\"value_format_name\":null,\"_kind_hint\":\"measure\",\"_type_hint\":\"number\",\"filter_expression\":\"${order_items.previous_period} = \\\"This Period\\\"\"},{\"measure\":\"previous_period\",\"based_on\":\"order_items.total_sale_price\",\"label\":\"Previous Period\",\"value_format\":null,\"value_format_name\":null,\"_kind_hint\":\"measure\",\"_type_hint\":\"number\",\"filter_expression\":\"${order_items.previous_period} = \\\"Previous Period\\\"\"},{\"table_calculation\":\"period_over_period\",\"label\":\"Period Over Period\",\"expression\":\"${this_period} / ${previous_period} - 1\",\"value_format\":null,\"value_format_name\":\"percent_1\",\"_kind_hint\":\"measure\",\"_type_hint\":\"number\"}]"
 }
 // field name we will want to extract for dropdown
 export const query_field_name = 'users.state'
 // map the dashboard date filter to the query date filter
 export const dashboard_date_filter = 'Date'
-export const query_date_filter = 'order_items.created_date'
+export const query_date_filter = 'order_items.previous_period_filter'
+export const query_calculation = 'period_over_period'
+
+export const logoUrl: string = 'https://lever-client-logos.s3.amazonaws.com/8409767c-5cbe-4597-9d88-193437980b30-1537395984831.png'
+
+// export const apiDropdownQuery = {
+
+//   "view": "order_items",
+//   "fields": [
+//       "this_period",
+//       "previous_period",
+//       "users.state"
+//   ],
+//   "filters": {
+//       "order_items.previous_period_filter": "30 days",
+//       "users.country": "USA"
+//   },
+//   "sorts": [
+//       "period_over_period desc"
+//   ],
+//   "limit": "500",
+//   "model": "thelook",
+//   "dynamic_fields": "[{\"measure\":\"this_period\",\"based_on\":\"order_items.total_sale_price\",\"label\":\"This Period\",\"value_format\":null,\"value_format_name\":null,\"_kind_hint\":\"measure\",\"_type_hint\":\"number\",\"filter_expression\":\"${order_items.previous_period} = \\\"This Period\\\"\"},{\"measure\":\"previous_period\",\"based_on\":\"order_items.total_sale_price\",\"label\":\"Previous Period\",\"value_format\":null,\"value_format_name\":null,\"_kind_hint\":\"measure\",\"_type_hint\":\"number\",\"filter_expression\":\"${order_items.previous_period} = \\\"Previous Period\\\"\"},{\"table_calculation\":\"period_over_period\",\"label\":\"Period Over Period\",\"expression\":\"${this_period} / ${previous_period} - 1\",\"value_format\":null,\"value_format_name\":\"percent_1\",\"_kind_hint\":\"measure\",\"_type_hint\":\"number\"}]"
+// }
