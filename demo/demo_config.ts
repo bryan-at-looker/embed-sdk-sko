7820366dd44de0806e86be47bf4fd142ff3effe5
Bryan Weber <bryanweber@15er.local>
2020-01-26 15:40:46 -0800

    remove the demo_user.json

--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -1,4 +1,4 @@
 // The address of your Looker instance. Required.
-export const lookerHost = 'saleseng.dev.looker.com'
+export const lookerHost = 'sko2020.dev.looker.com'
 // A dashboard that the user can see. Set to 0 to disable dashboard.
 export const dashboardId = 0
