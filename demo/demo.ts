03c7d2e208ef25e191a625be12542d3e7de4c85f
Spencer Wanlass <spencer.wanlass@gmail.com>
2020-01-28 23:23:32 -0800

    creating section3

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,5 +1,6 @@
 import { LookerEmbedSDK, LookerEmbedDashboard } from '../src/index'
 import { looker_host, dashboard_id } from './demo_config'
+import {  dashboard_state_filter } from './demo_config'
 
 const user = require('./demo_user.json')
 
@@ -7,7 +8,13 @@ const user = require('./demo_user.json')
 LookerEmbedSDK.init(looker_host, '/auth')
 
 const setupDashboard = async (dashboard: LookerEmbedDashboard) => {
-
+  const dropdown = document.getElementById('select-dropdown')
+  if (dropdown) {
+    dropdown.addEventListener('change', (event) => {
+      dashboard.updateFilters({ [dashboard_state_filter]: (event.target as HTMLSelectElement).value })
+      dashboard.run()
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -15,6 +22,9 @@ document.addEventListener('DOMContentLoaded', function () {
     LookerEmbedSDK.createDashboardWithId(dashboard_id)
       .appendTo('#dashboard')
       .withClassName('looker-embed')
+      .withFilters({[dashboard_state_filter]: 'California'})
+      .withTheme('sko')
+      .on('page:properties:changed', changeHeight )
       .build()
       .connect()
       .then(setupDashboard)
@@ -26,3 +36,11 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 })
+
+function changeHeight( event: any ) {
+  console.log(event)
+  const div = document.getElementById('dashboard')
+  if (event && event.height && div) {
+    div.style.height = `${event.height+15}px`
+  }
+}
--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -2,3 +2,5 @@
 export const looker_host = 'sko2020.dev.looker.com'
 // A dashboard that the user can see. Set to 0 to disable dashboard.
 export const dashboard_id = 5
+// the name of the filter on the dashboard for state
+export const dashboard_state_filter = 'State'
