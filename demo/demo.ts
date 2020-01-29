ae7371cd13ac3641f123a686687a3af477c8bef6
Spencer Wanlass <spencer.wanlass@gmail.com>
2020-01-29 00:10:01 -0800

    creating section5

--- demo/demo.ts
+++ demo/demo.ts
@@ -2,6 +2,8 @@ import { LookerEmbedSDK, LookerEmbedDashboard } from '../src/index'
 import { looker_host, dashboard_id } from './demo_config'
 import {  dashboard_state_filter } from './demo_config'
 import { LookerSDK, IApiSettings, AuthToken, IError, CorsSession } from '@looker/sdk'
+import { query_object, query_field_name } from './demo_config'
+import { dashboard_date_filter, query_date_filter } from './demo_config'
 
 let sdk: LookerSDK
 class EmbedSession extends CorsSession {
@@ -31,6 +33,13 @@ const setupDashboard = async (dashboard: LookerEmbedDashboard) => {
     })
   }
   const me = await sdk.ok(sdk.me())
+  const states = await sdk.ok(sdk.run_inline_query(
+    {
+      body: query_object,
+      result_format: 'json'
+    }
+  ))
+  addStateOptions(states)
   console.log('me', me)
 }
 
@@ -42,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
       .withFilters({[dashboard_state_filter]: 'California'})
       .withTheme('sko')
       .on('page:properties:changed', changeHeight )
+      .on('dashboard:filters:changed', filtersUpdates)
       .build()
       .connect()
       .then(setupDashboard)
@@ -59,5 +69,47 @@ function changeHeight( event: any ) {
   const div = document.getElementById('dashboard')
   if (event && event.height && div) {
     div.style.height = `${event.height+15}px`
+  }
+}
+
+function addStateOptions(data: any) {
+  const dropdown = document.getElementById('select-dropdown')
+  data.forEach(function (row: any, i: number) {
+    if (query_field_name && row[query_field_name] && dropdown) {
+      const new_option = document.createElement('option')
+      new_option.value = row[query_field_name]
+      new_option.innerHTML = `${i+1}) ${row[query_field_name]}`
+      dropdown.appendChild(new_option)
+
+      // replace or create the dropdown item
+      if (dropdown.children[i+1]) {
+        dropdown.children[i+1].replaceWith(new_option)
+      } else {
+        dropdown.appendChild(new_option)
+      }
+    }
+  })
+}
+
+async function filtersUpdates( event: any ) {
+  console.log('dashboard:filters:changed', event)
+  // instantiate elements, filters, and query objects
+  const dashboard_filters: any = (event && event.dashboard && event.dashboard.dashboard_filters) ? event.dashboard && event.dashboard.dashboard_filters : undefined
+  let dropdown = document.getElementById('select-dropdown')
+  let new_filters = query_object.filters
+
+  // update query object and run query
+
+  if (dashboard_filters && ( dashboard_date_filter in dashboard_filters ) ) { // check to make sure our filter is in the changed
+    if (dropdown) { // check to make sure we found our elements to update/keep
+      new_filters = Object.assign(new_filters, { [query_date_filter]: dashboard_filters[dashboard_date_filter] })
+      const states = await sdk.ok(sdk.run_inline_query(
+        {
+          body: Object.assign(query_object, { filters: new_filters }),
+          result_format: 'json'
+        }
+      ))
+      addStateOptions(states)
+    }
   }
 }
--- demo/demo_config.ts
+++ demo/demo_config.ts
@@ -4,3 +4,16 @@ export const looker_host = 'sko2020.dev.looker.com'
 export const dashboard_id = 5
 // the name of the filter on the dashboard for state
 export const dashboard_state_filter = 'State'
+export const query_object = {
+    "model": "thelook",
+    "view": "order_items",
+    "fields": ["users.state", "order_items.total_gross_margin"],
+    "sorts": ["order_items.total_gross_margin desc"],
+    "limit": "10",
+    "filters": {}
+  }
+// field name we will want to extract for dropdown
+export const query_field_name = 'users.state'
+// map the dashboard date filter to the query date filter
+export const dashboard_date_filter = 'Date'
+export const query_date_filter = 'order_items.created_date'
